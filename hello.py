61e60cf29e0905515a6a0bb408e45a850727aeb0
debashish <debashish350@gmail.com>
2017-11-26 18:10:51 +0000

    hello.py commit with templates

--- hello.py
+++ hello.py
@@ -1,14 +1,41 @@
 import os
-from flask import Flask
+from flask import Flask, url_for, request, render_template, redirect, flash    
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'your mom is lovely'
+
+#templates
+@app.route('/hello')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name_template=name)
+    
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method=='POST':
+        if valid_login(request.form['username'], request.form['password']):
+            flash("Successfully Loggen in")
+            return redirect(url_for('welcome', username=request.form.get('username')))
+        else:
+            error = 'Username and Password donot match'
+    return render_template('login.html', error=error)
+
+@app.route('/welcome/<username>')
+def welcome(username):
+    return render_template('welcome.html', username=username)
+
+def valid_login(username, password):
+    if username==password:
+        return True
+    else:
+        return False
     
 if __name__ == '__main__':
     host = os.getenv('IP', '0.0.0.0')
     port = int(os.getenv('PORT', 5000))
+    #setting degug is True, not for prod as it will slow down the app
+    app.debug = True
+    app.secret = 'secretkey982739821738238'
     app.run(host=host, port=port)
     
